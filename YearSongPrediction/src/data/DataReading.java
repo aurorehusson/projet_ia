e00f43c47858531d29cf16ec98d971679c1f2b42
Tiphaine <tiphaine.payen@telecomnancy.eu>
2017-12-07 12:47:30 +0100

    le code fonctionne mais la classification est mauvaise. Je suis à plus de 30 heures de boulot

--- YearSongPrediction/src/data/DataReading.java
+++ YearSongPrediction/src/data/DataReading.java
@@ -7,11 +7,14 @@ public class DataReading{
 	
 	static String fichier = "YearPredictionMSD.txt";
 	public ArrayList<ArrayList<Double>> dataTrain, dataTest;
+	public Double min, max;
 	
 	public void Lecture (){
+		min = Double.MAX_VALUE;
+		max = Double.MIN_VALUE;
+		dataTrain = new ArrayList<ArrayList<Double>>();
+		dataTest = new ArrayList<ArrayList<Double>>();
 		try{
-			dataTrain = new ArrayList<ArrayList<Double>>();
-			dataTest = new ArrayList<ArrayList<Double>>();
 			InputStream ips=new FileInputStream(fichier);
 			if(ips!= null){
 				InputStreamReader ipsr=new InputStreamReader(ips);
@@ -19,11 +22,19 @@ public class DataReading{
 				String ligne;
 				int i, j;
 				i = 0;
+				System.out.println("lecture");
 				while ((ligne=br.readLine())!=null){
+					
 					j = 0;
 					ArrayList<Double> simpleList = new ArrayList<Double>();
 					String[] elements = ligne.split(",");
 					while(j<elements.length){
+						if(new Double(elements[j])<min){
+							min = new Double(elements[j]);
+						}
+						if(new Double(elements[j])>max){
+							max = new Double(elements[j]);
+						}
 						simpleList.add(new Double(elements[j]));
 						j++;
 					}
--- YearSongPrediction/src/data/DataSorting.java
+++ YearSongPrediction/src/data/DataSorting.java
@@ -2,54 +2,86 @@ package data;
 
 import java.util.ArrayList;
 
+import org.omg.CORBA.DoubleSeqHelper;
+
 public class DataSorting {
 
-	public ArrayList<ArrayList<ArrayList<Double>>> dataTrainSort;
+	//public ArrayList<ArrayList<ArrayList<Double>>> dataTrainSort;
+	public ArrayList<Double[][]> dataTrainSort;
+	public ArrayList<ArrayList<Double>> dataTest;
+	public Double min, max;
+	
 	private ArrayList<ArrayList<Double>> dataTrain;
 	
 	public DataSorting(){
 		DataReading dr = new DataReading();
 		dr.Lecture();
 		this.dataTrain = dr.dataTrain;
+		this.dataTest=dr.dataTest;
+		this.min=dr.min;
+		this.max=dr.max;
 	}
 	
 	public void Tri(){
+		System.out.println("Tri");
 		//Initialisation
-		dataTrainSort = new ArrayList<ArrayList<ArrayList<Double>>>();
-		ArrayList<ArrayList<Double>> listeAnnee = new ArrayList<ArrayList<Double>>();
+		//dataTrainSort = new ArrayList<ArrayList<ArrayList<Double>>>();
+		dataTrainSort = new ArrayList<Double[][]>();
+		Double[][] listeAnnee = new Double[2][];
 		
 		//Initiailisation de la liste avec le premier element
 		Double annee = dataTrain.get(0).get(0);
-		ArrayList<Double> anneeListe = new ArrayList<Double>();
-		anneeListe.add(annee);
+		Double[] anneeListe = new Double[1];
+		anneeListe[0] = annee;
 		ArrayList<Double> element = dataTrain.get(0);
-		listeAnnee.add(anneeListe);
-		listeAnnee.add(element);
+		element.remove(0);
+		listeAnnee[0]=anneeListe;
+		Double[] elts = new Double[element.size()];
+		elts = element.toArray(elts);
+		listeAnnee[1]=elts;
 		dataTrainSort.add(listeAnnee);
 		
 		//Boucle sur tous les elements
 		int i, j;
 		for(i=1; i<dataTrain.size(); i++){
+			System.out.println("Tri :" +i);
 			annee = dataTrain.get(i).get(0);
 			j = 0;
 			boolean anneeTrouvee = false;
 			while(j<dataTrainSort.size() && anneeTrouvee == false){
-				if (dataTrainSort.get(j).get(0).get(0)==annee){
+				//System.out.println(annee);
+				//System.out.println(dataTrainSort.get(j)[0][0]);
+				if (dataTrainSort.get(j)[0][0].equals(annee)){
 					anneeTrouvee = true;
-					listeAnnee = dataTrainSort.get(j);
+					int n = dataTrainSort.get(j).length;
+					//System.out.println(n);
+					listeAnnee = new Double[n+1][];
+					//System.out.println(listeAnnee.length);
+					for(int indiceAnnee=0; indiceAnnee<n; indiceAnnee++){
+						listeAnnee[indiceAnnee] = dataTrainSort.get(j)[indiceAnnee];
+					}
+					//System.out.println(listeAnnee.length);
 					dataTrainSort.remove(j);
 					element = dataTrain.get(i);
-					listeAnnee.add(element);
+					element.remove(0);
+					elts = new Double[element.size()];
+					elts = element.toArray(elts);
+					//System.out.println(elts.length);
+					listeAnnee[n]=elts;
 					dataTrainSort.add(listeAnnee);
 				}
 				j++;
 			}
 			if(anneeTrouvee == false){
-				anneeListe = new ArrayList<Double>();
-				anneeListe.add(annee);
+				listeAnnee = new Double[2][];
+				anneeListe = new Double[1];
+				anneeListe[0] = annee;
 				element = dataTrain.get(i);
-				listeAnnee.add(anneeListe);
-				listeAnnee.add(element);
+				element.remove(0);
+				listeAnnee[0] = anneeListe;
+				elts = new Double[element.size()];
+				elts = element.toArray(elts);
+				listeAnnee[1] = elts;
 				dataTrainSort.add(listeAnnee);
 			}
 		}
